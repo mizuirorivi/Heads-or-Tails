078e6c3028de301428ffa5038ef6f88a2275ed25
mizuirorivi <onodaha@gmail.com>
2020-08-06 16:20:34 +0900

    Victory message added

--- test.cpp
+++ test.cpp
@@ -6,10 +6,11 @@ int main(){
     std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
     std::uniform_int_distribution<> rand100(0, 99);  // [0, 99] 範囲の一様乱数
     cout << "Tossing a coint..." << endl;
+    int h=0,t=0;
     for (int i = 0; i < 3; ++i) {
         cout << "Round " <<  i+1  << ":";
-        if(rand100(mt)&1) cout << "Heads" << endl;
-        else cout << "Tails" << endl;
+        if(rand100(mt)&1) cout << "Heads" << endl,h++;
+        else cout << "Tails" << endl,t++;
     }
-
+    cout << (h>t?"Yout won":"You lost") << endl;
 }
