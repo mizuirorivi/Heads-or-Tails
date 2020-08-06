c46cba60215b70cee420456324e64cff950c7efb
mizuirorivi <onodaha@gmail.com>
2020-08-06 16:11:29 +0900

    HoT:Initial revision

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    std::random_device rnd;     // 非決定的な乱数生成器を生成
+    std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
+    std::uniform_int_distribution<> rand100(0, 99);  // [0, 99] 範囲の一様乱数
+    cout << "Tossing a coint..." << endl;
+    for (int i = 0; i < 3; ++i) {
+        cout << "Round " <<  i+1  << ":";
+        if(rand100(mt)&1) cout << "Heads" << endl;
+        else cout << "Tails" << endl;
+    }
+
+}
